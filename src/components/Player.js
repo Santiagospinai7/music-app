de03e50e9fb16bafa4a4212702515342699bead1
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-04-13 00:12:22 +0200

    update time of the song

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,16 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon  } from "@fortawesome/react-fontawesome";
 import { faPlay, faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 
 const Player = ( { currentSong, isPlaying, setIsPlaying } ) => {
+  // State
+  const [songInfo, setSongInfo] = useState({ currentTime: 0, duration: 0 });
+
   // ref
   const audioRef = useRef(null);
 
-  // Funtions
+  // Functions
   const playSongHandler = () => {
     switch (isPlaying) {
       case true:
@@ -24,19 +27,32 @@ const Player = ( { currentSong, isPlaying, setIsPlaying } ) => {
     }
   };
 
+  const updateTimeHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    console.log(current, duration);
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+  };
+
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+
   return(
     <div className="player">
       <div className="time-control">
-        <p>Start Time</p>
+        <p>{ getTime(songInfo.currentTime) }</p>
         <input type="range" />
-        <p>End Time</p>
+        <p>{ getTime(songInfo.duration) }</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
         <FontAwesomeIcon onClick={playSongHandler} className="play" size="2x" icon={faPlay} />
         <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
       </div>
-      <audio ref={audioRef} src={currentSong.audio}></audio>
+      <audio onTimeUpdate={updateTimeHandler} onLoadedMetadata={updateTimeHandler} ref={audioRef} src={currentSong.audio}></audio>
     </div>
   )
 }

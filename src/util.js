86308012fa102e87f9e4f8d413e2581fcc2cd779
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-04-12 19:21:08 +0200

    add uuid package and add some songs to the app

---
+++ src/util.js
@@ -0,0 +1,66 @@
+import { v4 as uuidv4 } from 'uuid';
+
+function chillHop() {
+  return [
+    {
+      name: 'Beaver Creek',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/03/0a2a3e3b8c9e9a7f8c8c8d9d9d9d9d9d9d9d9d9d-1024x1024.jpg',
+      artist: 'Aso, Middle School, Aviino',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=8312',
+      color: ['#205950', '#2ab3bf'],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: 'Daylight',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/07/2b5f3e1c8e4c7f8d7a8b7d9d9d9d9d9d9d9d9d9d-1024x1024.jpg',
+      artist: 'Aiguille',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9272',
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Keep Going',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/07/2b5f3e1c8e4c7f8d7a8b7d9d9d9d9d9d9d9d9d9d-1024x1024.jpg',
+      artist: 'Swørn',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9222',
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Nightfall',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/07/2b5f3e1c8e4c7f8d7a8b7d9d9d9d9d9d9d9d9d9d-1024x1024.jpg',
+      artist: 'Sleepy Fish',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9148',
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Reflection',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/07/2b5f3e1c8e4c7f8d7a8b7d9d9d9d9d9d9d9d9d9d-1024x1024.jpg',
+      artist: 'Swørn',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9228',
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: 'Under the City Stars',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/07/2b5f3e1c8e4c7f8d7a8b7d9d9d9d9d9d9d9d9d9d-1024x1024.jpg',
+      artist: 'Philanthrope, chromonicci',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=9220',
+      color: ['#EF8EA9', '#ab417f'],
+      id: uuidv4(),
+      active: false,
+    },
+  ]
+}
